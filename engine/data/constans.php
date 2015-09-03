aa76a5dc5fb087bd0a066bba0978fbe2b67317cc
Tim Nijborg <timnijborg@hotmail.nl>
2015-09-03 12:20:36 +0200

    Create constant file

---
+++ engine/data/constans.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * All the constans data in one simple file
+ */
+
+// Database connection data
+define("DB_USERNAME", ""); // Database username
+define("DB_PASSWORD", ""); // Database password
+define("DB_HOST", "localhost"); // Database host
+define("DB_NAME", ""); // Database db name
+define("DB_PREFIX", ""); // Database table prefix
+
+// Global data
+define("GLOBAL_URL", ""); // Url, without https:// and / on the end
+define("GLOBAL_PATH", "/ExamenSoftware/public_html/ExamenSoftware/"); // Path of the files, empty is root
+define("GLOBAL_SEO", "false"); // Seo true/false
+
+
+?>
