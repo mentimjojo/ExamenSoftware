5da3a5b6345f77726c2a42ece90d62c1587cf759
Tim Nijborg <timnijborg@hotmail.nl>
2015-09-04 10:21:10 +0200

    Started connection class

---
+++ engine/controllers/database/connection.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Connection class
+ */
+class Connection {
+
+    // Public conn
+    public $conn;
+
+    // Function connection
+    function connectDB(){
+        try{
+            // Try to connect to the dababase
+            $this->conn = new PDO("mysql:host=".DB_HOST.";dbname=" . DB_NAME . ";", DB_USERNAME, DB_PASSWORD);
+            echo "Connnected";
+        } catch(PDOException $e){
+            // Throw error when not works
+            die("There is no database connection or some other error happend: " . $e->errorInfo);
+        }
+    }
+
+}
+?>
