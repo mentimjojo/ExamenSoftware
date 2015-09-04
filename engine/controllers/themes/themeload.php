49ca8425199679466b6625670256ef5edf2e97bd
Tim Nijborg <timnijborg@hotmail.nl>
2015-09-04 13:08:15 +0200

    Added theme load system

---
+++ engine/controllers/themes/themeload.php
@@ -0,0 +1,15 @@
+<?php
+// Theme load class
+class Themeload {
+
+    public function loadTheme(){
+        // Requiere theme
+        require_once("../".GLOBAL_PATH."/".GLOBAL_THEME."/index.php");
+    }
+
+    public function loadPageSystem(){
+
+    }
+}
+
+?>
