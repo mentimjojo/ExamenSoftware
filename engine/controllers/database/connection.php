ffcf3d7ecafaf8b5d2088040d1062c94cd459221
Tim Nijborg <timnijborg@hotmail.nl>
2015-09-04 12:02:07 +0200

    Added some comments

--- engine/controllers/database/connection.php
+++ engine/controllers/database/connection.php
@@ -12,6 +12,7 @@ class Connection {
         try{
             // Try to connect to the dababase
             $this->conn = new PDO("mysql:host=".DB_HOST.";dbname=" . DB_NAME . ";", DB_USERNAME, DB_PASSWORD);
+            // Show if it works.
             echo "Connnected";
         } catch(PDOException $e){
             // Throw error when not works
