f5f1fe16120733e660feab84a82d9ac84deb1a69
Tim Nijborg <timnijborg@hotmail.nl>
2015-09-04 10:21:30 +0200

    Added classes load functions

--- engine/initialize.php
+++ engine/initialize.php
@@ -1,3 +1,26 @@
 <?php
+/*
+ * Initialize file for getting all the classes etc.
+ */
 
+// Get constant data
+require_once("data/constans.php");
+
+// Load all the classes
+function LoadControllers($class){
+    // Load classes
+    foreach(glob("engine/controllers/**/".strtolower($class.".php")) as $filename){
+        // Include class
+        include($filename);
+    }
+}
+
+// Register load classes
+spl_autoload_register(LoadControllers);
+
+// Register connection class
+$Connection = new Connection();
+
+//  Make database connection
+$conn = $Connection->connectDB();
 ?>
